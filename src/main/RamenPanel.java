7ced148899f34bc84c89e0635ab15a76ff7b6091
sohee <16sseok@gmail.com>
2022-03-25 22:21:14 -0700

    updated

--- src/main/RamenPanel.java
+++ src/main/RamenPanel.java
@@ -1,11 +1,18 @@
 package main;
+import kitchen.Kitchen;
+import kitchen.Switch;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class RamenPanel extends JPanel {
-    public RamenPanel(Dimension dimension){
+    //field
+    private Kitchen kitchen;
+    private Switch aSwitch;
 
+    public RamenPanel(Dimension dimension){
+        kitchen = new Kitchen("assets/kitchen.png");
+        aSwitch = new Switch();
     }
 
     public void paintComponent(Graphics g) {
