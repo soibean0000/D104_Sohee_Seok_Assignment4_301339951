8b34b4fa0e4c76f987daaae47baf050ae4d6cc6b
석소희 <sohee@d207-023-223-010.wireless.sfu.ca>
2022-03-25 21:32:34 -0700

    Initial commit

---
+++ src/RamenApp.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class RamenApp extends JFrame {
+    public RamenApp(String title){
+        //constructor
+        super(title);	//that inherits from JFrame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1200,800);	//Panel size
+
+        //instantiating the FishPanel
+        RamenPanel panel = new RamenPanel(this.getSize());
+
+        //adding it to the current frame
+        this.add(panel);
+
+        //displaying the frame
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        RamenApp app = new RamenApp("My Ramen App");
+    }
+}
---
+++ src/RamenPanel.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RamenPanel extends JPanel {
+    public RamenPanel(Dimension dimension){
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+}
