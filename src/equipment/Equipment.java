2ffbd987ec9a9e446830ed38eb37474829f880a6
sohee <16sseok@gmail.com>
2022-03-27 15:00:48 -0700

    Added noodle, powder, powder, egg, and flakes

--- src/equipment/Equipment.java
+++ src/equipment/Equipment.java
@@ -7,8 +7,16 @@ import java.awt.image.BufferedImage;
 public class Equipment {
     //field 그릴것
     private BufferedImage equipImg;
+    private BufferedImage Noodle;
+    private BufferedImage Powder;
+    private BufferedImage Egg;
+    private BufferedImage Flakes;
 
     public Equipment(String file){
         equipImg = ImageLoader.loadImage(file);
+        Noodle = ImageLoader.loadImage(file);
+        Powder = ImageLoader.loadImage(file);
+        Egg = ImageLoader.loadImage(file);
+        Flakes = ImageLoader.loadImage(file);
     }
 }
