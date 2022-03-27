5f8b64801d405e29f385d33a6ef140c6e074d5bf
sohee <16sseok@gmail.com>
2022-03-27 15:03:03 -0700

    -

--- src/main/RamenPanel.java
+++ src/main/RamenPanel.java
@@ -9,10 +9,12 @@ public class RamenPanel extends JPanel {
     //field
     private Kitchen kitchen;
     private Switch aSwitch;
+    //private Switch stoveLever;
 
     public RamenPanel(Dimension dimension){
-        kitchen = new Kitchen("assets/kitchen.png");
+        kitchen = new Kitchen("assets/KitchenImg.png");
         aSwitch = new Switch();
+        //stoveLever = new stoveLever();
     }
 
     public void paintComponent(Graphics g) {
