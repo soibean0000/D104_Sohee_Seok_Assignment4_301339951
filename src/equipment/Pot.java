69cb4a307883f48190847363755fe1e0ea837909
sohee <16sseok@gmail.com>
2022-03-28 05:12:53 -0700

    pot with bounding box & state when the water filled in

--- src/equipment/Pot.java
+++ src/equipment/Pot.java
@@ -5,4 +5,23 @@ import java.awt.geom.AffineTransform;
 
 public class Pot extends Equipment{
 
+    public Pot(String file, double scale, int posX, int posY) {
+        super(file, scale, posX, posY);
+    }
+
+    public void draw(Graphics2D g){
+        g.draw(boundBox);
+        AffineTransform at = g.getTransform();
+        g.drawImage(img, xPos, yPos, (int) (img.getWidth()*scale), (int) (img.getHeight()*scale), null);
+        g.setTransform(at);
+    }
+
+
+    @Override
+    public void setState(int state){
+        super.setState(state);
+        if(state == 1){
+            setImg("assets/FireOn.png");  //pot with water in
+        }
+    }
 }
